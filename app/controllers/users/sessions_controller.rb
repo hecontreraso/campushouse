ea30824f5582fa1a6f607d0f5fae24dcfcdbacc2
Esteban <hecontreraso@gmail.com>
2016-01-01 23:35:47 -0500

    Fixed redirection when login fails

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
+  # respond_to :html, :json
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -6,10 +7,16 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
+  # POST /resource/sign_in
   def create
-    self.resource = warden.authenticate!(auth_options)
+    resource = warden.authenticate!(scope: resource_name, recall: "#{controller_path}#failure")
     set_flash_message(:notice, :signed_in) if is_flashing_format?
     sign_in(resource_name, resource)
+    respond_with resource, location: request.referer
+  end
+
+  def failure
+    flash[:alert] = I18n.t("user.not_found_in_database")
     respond_with resource, location: request.referer
   end
 
@@ -25,3 +32,14 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.for(:sign_in) << :attribute
   # end
 end
+
+  # respond_to :html, :json
+  # def create
+  #   resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#failure")
+  #   sign_in(resource_name, resource)
+  #   return render :json => {:success => true, location: after_sign_in_path_for(resource), token: form_authenticity_token}
+  # end
+
+  # def failure
+  #   return render :json => {error_msg: "Login details incorrect."}, status: 422
+  # end
